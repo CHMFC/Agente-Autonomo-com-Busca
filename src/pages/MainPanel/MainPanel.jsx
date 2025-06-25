e0e6056bd2cec269c09bece2964ccc5bbe2ab1b4
CHMFC <chmfc@cin.ufpe.br>
2025-06-25 15:31:02 -0300

    adicionando os participantes

--- src/pages/MainPanel/MainPanel.jsx
+++ src/pages/MainPanel/MainPanel.jsx
@@ -1,10 +1,9 @@
-// src/pages/MainPanel/MainPanel.jsx
 import React from 'react';
 import ControlPanel from '../../components/ControlPanel/ControlPanel';
 import './MainPanel.css';
 
 function MainPanel(props) {
-  // --- GARANTINDO QUE AS PROPS SÃO RECEBIDAS E REPASSADAS ---
+  // Garantimos que TODAS as props, incluindo as novas, são recebidas.
   const { 
     onStart, 
     onReset, 
@@ -13,12 +12,15 @@ function MainPanel(props) {
     gameState,
     animationSpeed,
     onSpeedChange,
+    terrainCosts,
+    onCostChange,
   } = props;
 
   return (
     <div className="main-panel-container">
       <h1>Visualizador de Busca</h1>
       <ControlPanel
+        // ... e repassadas para o ControlPanel
         onStart={onStart}
         onReset={onReset}
         onAlgoChange={onAlgoChange}
@@ -26,10 +28,17 @@ function MainPanel(props) {
         gameState={gameState}
         animationSpeed={animationSpeed}
         onSpeedChange={onSpeedChange}
+        terrainCosts={terrainCosts}
+        onCostChange={onCostChange}
       />
       <div className="description">
-        <p>Selecione um algoritmo de busca e clique em "Iniciar Busca" para ver o agente encontrar o caminho.</p>
-        <p>Use "Gerar Novo Mapa" para testar o algoritmo em diferentes cenários aleatórios.</p>
+        <p>Selecione um algoritmo de busca e clique em "Iniciar Busca" para ver o agente encontrar o caminho. Use "Gerar Novo Mapa" para testar o algoritmo em diferentes cenários aleatórios.</p>
+        <p><b>Grupo formado por:</b><br></br>
+            Carlos Henrique Matos Fontaine Costa <br></br>
+            Lucas Emmanuel Gomes de Lucena <br></br>
+            Nicolas Cavalcanti de Brito Barros <br></br>
+            Rômulo Artur Arruda dos Santos Andrade <br></br>
+            Vitor Negromonte Cabral de Oliveira</p>
       </div>
     </div>
   );
